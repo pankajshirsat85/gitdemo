2691949e4ef01c1b1cc1b4721680ab5a0db86c7b
pankaj <pankajshirsat85@gmail.com>
2026-08-12 23:12:32 +0530

    second commit

---
+++ test2.js
@@ -0,0 +1,12 @@
+//this is dummy test file
+ //…or create a new repository on the command line
+//echo "# gitdemo" >> README.md
+//git init`
+//git add README.md
+//git commit -m "first commit"
+//git branch -M main
+//git remote add origin 
+// https://github.com/pankajshirsat85/gitdemo.git
+//git push -u origin main
+
+
